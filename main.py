af5e493a4576345541c67cc74bc70f4d081ede0f
Rayan Zouba <rayan.zouba@edu.univ-eiffel.fr>
2025-11-24 12:22:04 +0100

    france a l'envers

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from shapefile import *
+from Requirement.fltk import *
 
 sf = Reader("departements_20180101")
 
-sf.records()
+cree_fenetre(1000,1000)
 
-print(sf)
+print(sf.record)
+
+seine_et_marne = sf.shape(47)
+seine_et_marne = sf.shape(47)
+
+for i in range(100):
+    departement = sf.shape(i)
+    for j in range(len(departement.points)):
+        polygone((departement.points[j][0]*20,departement.points[j][1]*20),couleur="black")
+
+attend_ev()
+
+ferme_fenetre()
